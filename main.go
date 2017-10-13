61b1d852fcceab127c93a01576005b254a590396
Danny Brody <danny.brody@sisa.samsung.com>
2017-10-13 13:07:56 -0700

    adding sample test

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
     "log"
 )
 
+// e.g. http.HandleFunc("/health-check", HealthCheckHandler)
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+    // A very simple health check.
+    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "application/json")
+
+    // In the future we could report back on the status of our DB, or our cache 
+    // (e.g. Redis) by performing a simple PING, and include them in the response.
+    io.WriteString(w, `{"alive": true}`)
+}
+
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
     r.ParseForm()  // parse arguments, you have to call this by yourself
     fmt.Println(r.Form)  // print form information in server side
@@ -22,6 +33,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", sayhelloName) // set router
+    http.HandleFunc("/health-check", HealthCheckHandler) // set router
     err := http.ListenAndServe(":8081", nil) // set listen port
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
