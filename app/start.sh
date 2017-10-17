311bf887da46034b5ff892249c9c4b9443a2ca0e
Danny Brody <danny.brody@sisa.samsung.com>
2017-10-17 10:21:47 -0700

    changing log paths

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
-nohup /home/ubuntu/gocode/src/app/app > out 2>error &
+nohup /home/ubuntu/gocode/src/app/app > /home/ubuntu/out 2> /home/ubuntu/error &
 echo $! > /home/ubuntu/app.pid
