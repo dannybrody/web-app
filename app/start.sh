cf2801f607f92ea3e2433c8f209fec24d06c583f
Danny Brody <danny.brody@sisa.samsung.com>
2017-10-17 12:37:41 -0700

    fixing start

--- app/start.sh
+++ app/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -ex
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=/home/ubuntu/gocode
+cd /home/ubuntu/gocode/src/app/
+rm -r vendor
+go get github.com/astaxie/beego
+go get
+go build
 nohup /home/ubuntu/gocode/src/app/app > /home/ubuntu/out 2> /home/ubuntu/error &
 echo $! > /home/ubuntu/app.pid
