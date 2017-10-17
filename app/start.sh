a8f839b5c8e694aab395e3c73386b83b7d4dc8f0
Danny Brody <danny.brody@sisa.samsung.com>
2017-10-17 11:32:05 -0700

    updating start

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
+export GOPATH=/home/ubuntu/gocode/
+go get github.com/astaxie/beego
 nohup /home/ubuntu/gocode/src/app/app > /home/ubuntu/out 2> /home/ubuntu/error &
 echo $! > /home/ubuntu/app.pid
