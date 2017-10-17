5b67c1152edc0d09e00809320bb94c3d95c01247
Danny Brody <danny.brody@sisa.samsung.com>
2017-10-17 12:45:21 -0700

    debug

--- app/start.sh
+++ app/start.sh
@@ -2,8 +2,9 @@
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
 cd /home/ubuntu/gocode/src/app/
 rm -r vendor
-go get github.com/astaxie/beego
-go get
-go build
+go env
+# go get github.com/astaxie/beego
+# go get
+# go build
 nohup /home/ubuntu/gocode/src/app/app > /home/ubuntu/out 2> /home/ubuntu/error &
 echo $! > /home/ubuntu/app.pid
